149dd6aa46be3414cd2c7046b4941a7f031b80ae
arurian <arunvb2004@gmail.com>
2020-11-21 13:47:18 +1030

    Initial commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,25 @@
+### Schema
+
 USE thjgsjh6mvck6pc7;
 
-
-CREATE TABLE farmer (
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE farmer (  
+    id int NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL ,
     last_name VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE TABLE products (
+    id int NOT NULL AUTO_INCREMENT,
+	product_name VARCHAR(50) NOT NULL,
+    product_availability BOOLEAN NOT NULL,
+    farmer_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (farmer_id) REFERENCES farmer(id)
+);
+
+-- Updates the row where the column name is products --
+UPDATE products
+SET product_availability = true, product_name = "Carrot"
+WHERE id = 1;
+
+SELECT * FROM products;
