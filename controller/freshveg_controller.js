140635d0235c39349cbad5ef9ae70550a5aa6c92
Timothy Forbes <timothy.forbes259@gmail.com>
2020-11-30 18:35:41 +1030

    added try error staements

--- controller/freshveg_controller.js
+++ controller/freshveg_controller.js
@@ -17,29 +17,30 @@ const { Op } = require("sequelize");
 const products = require("../models/products");
 
 router.get("/", (req, res) =>{
-    const products = db.products.findAll();
-      /*   console.log(products);
-        console.log(db.products); */
-       db.products.findAll().then(function(data) {
-            /*    console.log(data); */
-               let hdbrsObj = {
+    try { 
+        db.products.findAll().then(function(data) {
+            
+            let hdbrsObj = {
                 products: data
-               };
-               res.render("vege", hdbrsObj);
-             });
+            };
+                res.render("vege", hdbrsObj);
+            });
+    } catch(error) {
+        res.status(500).send('Error, something is not working please try again.')
+    }
 });
  // route for an individual farmer's page
  router.get("/farmer", (req, res) =>{
-/*      const farmers = db.farmers.findAll(); */
- /*     console.log(farmers);
-     console.log(db.farmers); */
-    db.farmers.findAll().then(function(data) {
-           /*  console.log(data); */
+    try {
+        db.farmers.findAll().then(function(data) {
+           
            let vegeArray = ['tomato','potato','onion','brocoli','pineapple','avocado','mango','banana','pomegranate'];
 
            //randomised vege Value
            let selector = rando(0, (vegeArray.length-1))
+
            let selectedVege = vegeArray[selector]
+
             let hdbrsObj = {
               farmers: data,
               product_name: selectedVege
@@ -47,10 +48,14 @@ router.get("/", (req, res) =>{
 
 
             res.render("index", hdbrsObj);
-          });
+        });
+    } catch (error) {
+        res.status(500).send('Error, could not access vege database.')
+    }
     });
  
 router.get("/farmer/:sales", (req, res) =>{
+    try {
     let vegeparameter =  req.params.sales
 
         db.sales.findOne({
@@ -62,25 +67,20 @@ router.get("/farmer/:sales", (req, res) =>{
                 }
             }
         }).then(function(data) {
-            /*  console.log(data); */
+            
              let vegeUppercase = vegeparameter.toUpperCase();
+
              let hdbrsObj = {
              sales: data,
              product_name: vegeUppercase
              };
-            res.render("sales", hdbrsObj);
+             
+            res.render("sales", hdbrsObj)
         });
+    } catch (error) {
+        res.status(500).send('Error, selected vegetable could not load, either the server is down for maintenance or the desired produce is no longer for sale.')
+    }
 });
-    
-// API Routes
-// =====================================================
-router.get("/api/farmer"), (req,res) => {
-    // api get request to call in farmer table information to create info cards
-    farmers.findAll({}).then((results) => {
-        res.json(results)
-    })
-}
-
 
 module.exports = router;
 
