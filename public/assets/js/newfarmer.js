3ad3d61f35058dcd00350d36ee98d49e29825e89
Timothy Forbes <timothy.forbes259@gmail.com>
2020-12-01 11:40:37 +1030

    forntend acknowledgement of updated farmer group and form clear function

--- public/assets/js/newfarmer.js
+++ public/assets/js/newfarmer.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 
     $("#submitBtn").on('click', function(event) {
         event.preventDefault();
-
+        //statement check to ensure fields are empty, both need to be filled
+        try {
         $.ajax("/api/addvege", {
             type: 'POST',
             data: {
@@ -11,9 +12,24 @@ $(document).ready(function() {
             },
           }).then(function(res) {
               console.log(`New farmer ${res} added!`);
+
+            //clear input fields
+            clearForm();
+              
+              //notify user if successful
+              alert(`Successfully added ${res} to the database`);
+              
           })
+        } catch (error) {
+            throw error;
+        }
     });
 
+    clearForm = () => {
+        $("#first_name").replace($("#first_name").val(), '')
+
+        $("#last_name").replace($("#first_name").val(), '')  
+    }
 
     
 
